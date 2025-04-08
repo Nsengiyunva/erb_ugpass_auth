f4cb61deee749918428cf168535dacacead19f47
Nsengiyunva <kingbecks07@gmail.com>
2025-04-08 17:00:41 +0300

    adds the production creds

--- routes/index.js
+++ routes/index.js
@@ -24,8 +24,12 @@ const aud = `https://ugpass.go.ug/idp/api/Authentication/token`
 const ughubaseUrl = `https://api-uat.integration.go.ug/t/nita.go.ug/daes/1.0.0/idp`
 const ughubtokenUrl = `https://api-uat.integration.go.ug`
 const proxyUrl = `https://intra.works.go.ug`
-const signbaseUrl = `https://api-uat.integration.go.ug/t/nita.go.ug/daes/1.0.0/signingservice/SignatureWebService/`
-const verificationbaseUrl = `https://stgapi.ugpass.go.ug/signing-service/SignatureWebService` 
+
+// const signbaseUrl = `https://api-uat.integration.go.ug/t/nita.go.ug/daes/1.0.0/signingservice/SignatureWebService/`
+const signbaseUrl = `https://integration.go.ug/t/nita.go.ug/daes/1.0.0/signingservice/SignatureWebService/`
+
+// const verificationbaseUrl = `https://stgapi.ugpass.go.ug/signing-service/SignatureWebService` 
+const verificationbaseUrl = `https://ugpass.go.ug/signing-service/SignatureWebService` 
 
 // const clientID = `aUdRFvdXrSQGmmemdFXDdD6S65lrMFFHBOMxMRLP50MJL9MD`
 // const clientSecret = `6mM1PXYsD9tyEj0Iap9B028cP8oCgB8a4uM6vWUKDFbakKjw5xhhfE5feHSw0erJ`
