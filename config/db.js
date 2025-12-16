826e3a9cecf20d50e9702349ef067b680730d4a8
Nsengiyunva <kingbecks07@gmail.com>
2025-12-16 15:40:37 +0300

    undockering

--- config/db.js
+++ config/db.js
@@ -25,18 +25,27 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASS,
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "mysql",
+//     dialectOptions: {
+//       ssl: false,
+//     },
+//     logging: true,
+//   }
+// );
+export const sequelize = new Sequelize(
+  "erbdb",
+  "erbadmin",
+  "admin@NSE#256",
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
-    dialectOptions: {
-      ssl: false,
-    },
-    logging: true,
+    host: "localhost",
+    dialect: "mysql"
   }
 );
 
