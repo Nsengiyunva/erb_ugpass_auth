c1c1e83a29fd6b9dd29fc4fc22e4f62683edaf93
Nsengiyunva <kingbecks07@gmail.com>
2025-12-16 15:51:17 +0300

    updates the erb auth

--- config/db.js
+++ config/db.js
@@ -74,43 +74,26 @@
 
 
 import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
-dotenv.config();
 
-// Use .env values with fallback defaults
 export const sequelize = new Sequelize(
-  process.env.DB_NAME || "erbdb",
-  process.env.DB_USER || "erbadmin",
-  process.env.DB_PASSWORD || "admin@NSE#256",
+  "erbdb",
+  "erbadmin",
+  "admin@NSE#256",
   {
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: parseInt(process.env.DB_PORT, 10) || 3306,
-    dialect: "mysql",
-    dialectOptions: {
-      ssl: false
-    },
-    logging: true
+    host: "localhost",
+    dialect: "mysql"
   }
 );
 
-// Retry logic
-export const connectWithRetry = async (retries = 10, delay = 5000) => {
-  while (retries) {
-    try {
-      console.log("🟢 Trying to connect to database...");
-      await sequelize.authenticate();
-      console.log("✅ Database connection established successfully.");
-      return true;
-    } catch (err) {
-      console.error(`❌ Database connection failed: ${err.message}`);
-      retries -= 1;
-      if (!retries) {
-        console.error("🚨 Out of retries. Exiting.");
-        process.exit(1);
-      }
-      console.log(`🔄 Retrying in ${delay / 1000}s... (${retries} attempts left)`);
-      await new Promise((res) => setTimeout(res, delay));
-    }
+export const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("ERB Database connected");
+
+    await sequelize.sync({ alter: true });
+    console.log("All models synchronized...");
+
+  } catch (error) {
+    console.error("Unable to start server:", error);
   }
 };
-
--- server.js
+++ server.js
@@ -1,167 +1,209 @@
+// import express from "express";
+// import * as dotenv from 'dotenv'
+// import router from './routes/index.js'
+// import authRoutes from './routes/auth.js'
+// import { sendStyledMail, sendEmailsInChunks } from "./mailer.js";
+// import path from "path";
+
+// import { sequelize, connectWithRetry } from "./config/db.js";
+
+// import IORedis from 'ioredis'
+// import bodyParser from "body-parser";
+// import { Queue } from "bullmq";
+// import client from 'prom-client'
+
+
+// dotenv.config();
+
+// const uploadDir = path.join(process.cwd(), "uploads");
+
+// const app = express();
+
+// app.use(express.json({ limit: "200mb" }));
+// app.use(express.urlencoded({ extended: true, limit: "200mb" }));
+// app.use(bodyParser.json())
+
+
+// client.collectDefaultMetrics();
+
+// const httpRequestCounter = new client.Counter({
+//   name: 'http_requests_total',
+//   help: 'Total number of HTTP requests',
+//   labelNames: ['method', 'route', 'status_code'],
+// });
+
+// const redisConnection  =  new IORedis( {
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT
+// } )
+
+// const PORT = process.env.PORT || 8754;
+
+// const startServer = async () => {
+//   await connectWithRetry(); 
+
+//   await sequelize.sync({ alter: true }); 
+
+//     app.use(function (_, res, next) {
+
+//       res.header("Access-Control-Allow-Origin", "*"); // or "*" if you must
+//       res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+//       res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+//       res.header("Access-Control-Allow-Credentials", "true");
+//       next();
+//     });
+//     app.use( express.json() )
+
+//     // Count all HTTP requests except /api/metrics
+//     app.use((req, res, next) => {
+//       if (req.path === '/api/metrics') return next();
+
+//       res.on('finish', () => {
+//         let routeLabel = req.route?.path || req.path;
+//         httpRequestCounter.labels(req.method, routeLabel, res.statusCode).inc();
+//       });
+//       next();
+//     } );
+
+
+//     app.get('/api/metrics', async (req, res) => {
+//       res.set('Content-Type', client.register.contentType);
+//       res.end(await client.register.metrics());
+//     });
+
+
+//     app.use("/auth/api", authRoutes )
+//     app.use( "/api", router )
+
+//     app.use("/uploads", express.static(uploadDir));
+
+//     const emailQueue = new Queue( "email_queue", {
+//       connection: redisConnection
+//     } )
+    
+//     app.get("/email/send", async (_, res) => {
+
+//       const htmlContent = `
+//       <div style="font-family: Arial, sans-serif; background-color: #f6f9fc; padding: 20px;">
+//         <div style="max-width: 600px; background: white; border-radius: 10px; padding: 20px; margin: auto; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+//           <h2 style="color: #007bff;">Hello, King Isaac 👋</h2>
+//           <p style="font-size: 16px; color: #333;">
+//             This is a <strong>styled email</strong> sent from your Node.js API.
+//           </p>
+//           <p style="font-size: 15px; color: #666;">
+//             It supports full HTML and inline CSS — so you can design beautiful email templates.
+//           </p>
+//           <a href="https://www.erb.go.ug" 
+//             style="display:inline-block; margin-top:20px; background-color:#007bff; color:white; padding:10px 20px; text-decoration:none; border-radius:5px;">
+//             Visit ERB Website
+//           </a>
+//           <p style="margin-top:30px; font-size:13px; color:#999;">© 2025 ERB Uganda</p>
+//         </div>
+//       </div>
+//     `;
+
+//       try {
+//         // await sendMail("isaacnsengiyunva@gmail.com", "Hello!", "This is a test email please.");
+//         await sendStyledMail("isaacnsengiyunva@gmail.com", "Email Test", htmlContent);
+//         res.json({ message: "Email sent successfully!" });
+//       } catch (err) {
+//         // console.error( "failed to send email", err );
+//         res.status(500).json({ error: "Failed to send email" });
+//       }
+//     });
+
+//     app.post("/send-batch", async (req, res) => {
+//       const { emails } = req.body;
+
+//       const htmlContent = `
+//       <div style="font-family: Arial, sans-serif; background-color: #f6f9fc; padding: 20px;">
+//         <div style="max-width: 600px; background: white; border-radius: 10px; padding: 20px; margin: auto; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+//           <h2 style="color: #007bff;">Hello, King Isaac 👋</h2>
+//           <p style="font-size: 16px; color: #333;">
+//             This is a <strong>styled email</strong> sent from your Node.js API.
+//           </p>
+//           <p style="font-size: 15px; color: #666;">
+//             It supports full HTML and inline CSS — so you can design beautiful email templates.
+//           </p>
+//           <a href="https://www.erb.go.ug" 
+//             style="display:inline-block; margin-top:20px; background-color:#007bff; color:white; padding:10px 20px; text-decoration:none; border-radius:5px;">
+//             Visit ERB Website
+//           </a>
+//           <p style="margin-top:30px; font-size:13px; color:#999;">© 2025 ERB Uganda</p>
+//         </div>
+//       </div>
+//     `;
+
+//       if (!emails || !Array.isArray(emails) || emails.length === 0) {
+//         return res.status(400).json({ message: "Emails array is required" });
+//       }
+
+//       let subject = "RE: ERB TEST MAIL:";
+
+//       try {
+//         await sendEmailsInChunks(emails, subject, htmlContent, {
+//           chunkSize: 50,
+//           retryLimit: 3,
+//           rateLimitPerSec: 5, // adjust as needed
+//         });
+//         // let subject = "RE: ERB TEST MAILS";
+//         // await emailQueue.add( "batch_email", { emails, subject, htmlContent } )
+
+//         res.status( 202 ).json( {
+//           message: "Emails queued successfully",
+//           count: emails.length
+//         } )
+
+//       } catch (err) {
+//         res.status(500).json({ message: "Failed to send some emails", error: err.message });
+//       }
+//     });
+
+//     app.listen(PORT, () => {
+//       console.log(`The Server is running on port: ${PORT}`);
+//     });
+// };
+
+// startServer();
+
+
+// import express from "express";
+// import dotenv from "dotenv";
+// import engineers_routes  from "./routes/engineer_routes"
+
+
+
 import express from "express";
 import * as dotenv from 'dotenv'
 import router from './routes/index.js'
 import authRoutes from './routes/auth.js'
-import { sendStyledMail, sendEmailsInChunks } from "./mailer.js";
-import path from "path";
-
-import { sequelize, connectWithRetry } from "./config/db.js";
-
-import IORedis from 'ioredis'
-import bodyParser from "body-parser";
-import { Queue } from "bullmq";
-import client from 'prom-client'
-
+// import { sendStyledMail, sendEmailsInChunks } from "./mailer.js";
+// import path from "path";
+import { sequelize, connectDB } from "./config/database";
+// import cors from "cors";
 
 dotenv.config();
 
-const uploadDir = path.join(process.cwd(), "uploads");
-
 const app = express();
+const PORT = process.env.PORT || 8754;
+   
+app.use(express.json());
 
-app.use(express.json({ limit: "200mb" }));
-app.use(express.urlencoded({ extended: true, limit: "200mb" }));
-app.use(bodyParser.json())
-
-
-client.collectDefaultMetrics();
-
-const httpRequestCounter = new client.Counter({
-  name: 'http_requests_total',
-  help: 'Total number of HTTP requests',
-  labelNames: ['method', 'route', 'status_code'],
+app.use(function (_, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
 });
 
-const redisConnection  =  new IORedis( {
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
-} )
+  app.use( express.json() )
 
-const PORT = process.env.PORT || 8754;
-
-const startServer = async () => {
-  await connectWithRetry(); 
-
-  await sequelize.sync({ alter: true }); 
-
-    app.use(function (_, res, next) {
-
-      res.header("Access-Control-Allow-Origin", "*"); // or "*" if you must
-      res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-      res.header("Access-Control-Allow-Credentials", "true");
-      next();
-    });
-    app.use( express.json() )
-
-    // Count all HTTP requests except /api/metrics
-    app.use((req, res, next) => {
-      if (req.path === '/api/metrics') return next();
-
-      res.on('finish', () => {
-        let routeLabel = req.route?.path || req.path;
-        httpRequestCounter.labels(req.method, routeLabel, res.statusCode).inc();
-      });
-      next();
-    } );
+  app.use("/auth/api", authRoutes )
+  app.use( "/api", router )
 
 
-    app.get('/api/metrics', async (req, res) => {
-      res.set('Content-Type', client.register.contentType);
-      res.end(await client.register.metrics());
-    });
+connectDB();
+sequelize.sync().then(() => console.log("ERB Tables synced..."));
 
-
-    app.use("/auth/api", authRoutes )
-    app.use( "/api", router )
-
-    app.use("/uploads", express.static(uploadDir));
-
-    const emailQueue = new Queue( "email_queue", {
-      connection: redisConnection
-    } )
-    
-    app.get("/email/send", async (_, res) => {
-
-      const htmlContent = `
-      <div style="font-family: Arial, sans-serif; background-color: #f6f9fc; padding: 20px;">
-        <div style="max-width: 600px; background: white; border-radius: 10px; padding: 20px; margin: auto; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
-          <h2 style="color: #007bff;">Hello, King Isaac 👋</h2>
-          <p style="font-size: 16px; color: #333;">
-            This is a <strong>styled email</strong> sent from your Node.js API.
-          </p>
-          <p style="font-size: 15px; color: #666;">
-            It supports full HTML and inline CSS — so you can design beautiful email templates.
-          </p>
-          <a href="https://www.erb.go.ug" 
-            style="display:inline-block; margin-top:20px; background-color:#007bff; color:white; padding:10px 20px; text-decoration:none; border-radius:5px;">
-            Visit ERB Website
-          </a>
-          <p style="margin-top:30px; font-size:13px; color:#999;">© 2025 ERB Uganda</p>
-        </div>
-      </div>
-    `;
-
-      try {
-        // await sendMail("isaacnsengiyunva@gmail.com", "Hello!", "This is a test email please.");
-        await sendStyledMail("isaacnsengiyunva@gmail.com", "Email Test", htmlContent);
-        res.json({ message: "Email sent successfully!" });
-      } catch (err) {
-        // console.error( "failed to send email", err );
-        res.status(500).json({ error: "Failed to send email" });
-      }
-    });
-
-    app.post("/send-batch", async (req, res) => {
-      const { emails } = req.body;
-
-      const htmlContent = `
-      <div style="font-family: Arial, sans-serif; background-color: #f6f9fc; padding: 20px;">
-        <div style="max-width: 600px; background: white; border-radius: 10px; padding: 20px; margin: auto; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
-          <h2 style="color: #007bff;">Hello, King Isaac 👋</h2>
-          <p style="font-size: 16px; color: #333;">
-            This is a <strong>styled email</strong> sent from your Node.js API.
-          </p>
-          <p style="font-size: 15px; color: #666;">
-            It supports full HTML and inline CSS — so you can design beautiful email templates.
-          </p>
-          <a href="https://www.erb.go.ug" 
-            style="display:inline-block; margin-top:20px; background-color:#007bff; color:white; padding:10px 20px; text-decoration:none; border-radius:5px;">
-            Visit ERB Website
-          </a>
-          <p style="margin-top:30px; font-size:13px; color:#999;">© 2025 ERB Uganda</p>
-        </div>
-      </div>
-    `;
-
-      if (!emails || !Array.isArray(emails) || emails.length === 0) {
-        return res.status(400).json({ message: "Emails array is required" });
-      }
-
-      let subject = "RE: ERB TEST MAIL:";
-
-      try {
-        await sendEmailsInChunks(emails, subject, htmlContent, {
-          chunkSize: 50,
-          retryLimit: 3,
-          rateLimitPerSec: 5, // adjust as needed
-        });
-        // let subject = "RE: ERB TEST MAILS";
-        // await emailQueue.add( "batch_email", { emails, subject, htmlContent } )
-
-        res.status( 202 ).json( {
-          message: "Emails queued successfully",
-          count: emails.length
-        } )
-
-      } catch (err) {
-        res.status(500).json({ message: "Failed to send some emails", error: err.message });
-      }
-    });
-
-    app.listen(PORT, () => {
-      console.log(`The Server is running on port: ${PORT}`);
-    });
-};
-
-startServer();
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
