e53da2b6983499cdbc5afc29669180dcf0758d2d
Nsengiyunva <kingbecks07@gmail.com>
2025-12-18 13:27:52 +0300

    updates  the app

--- routes/index.js
+++ routes/index.js
@@ -377,7 +377,7 @@ router.post( `/add_qr_code`,  async( req, res  ) => {
 router.post( `/bulk-sign`, async(req,  res ) =>  {
     try {
         let { access_token, email_address, role  }  = req.body
-        let correlationId = "7462372";
+        let correlationId = "17678670";
         let data = new FormData();
 
         if (!access_token) {
