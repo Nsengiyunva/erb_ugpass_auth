11efb64c4182065dc2edbea01d182641ba3cbe5d
Nsengiyunva <kingbecks07@gmail.com>
2026-02-04 10:59:49 +0300

    updates  the app

--- routes/index.js
+++ routes/index.js
@@ -607,7 +607,7 @@ router.get('/verify_license/:license_no', authMiddleware, async (req, res) => {
   
       return res.status(200).json(response);
     } catch (error) {
-    //   console.error(error);
+      console.error("error+",error);
       return res.status(500).json({
         message: 'Internal server error',
       });
